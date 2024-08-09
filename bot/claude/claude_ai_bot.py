004901b1dc3e3d3c52750761cd46af6182658420
Larry Zhu <larryzhu@foxmail.com>
2024-08-09 18:11:17 +0800

    feat: support Coze

--- bot/claude/claude_ai_bot.py
+++ bot/claude/claude_ai_bot.py
@@ -195,7 +195,7 @@ class ClaudeAIBot(Bot, OpenAIImage):
                     logger.error("rate limit error: The conversation has reached the system speed limit and is synchronized with Cladue. Please go to the official website to check the lifting time")
                     return Reply(ReplyType.ERROR, "对话达到系统速率限制，与cladue同步，请进入官网查看解除限制时间")
                 logger.info(f"[CLAUDE] reply={reply_content}, total_tokens=invisible")
-                self.sessions.session_reply(reply_content, session_id, 100)
+                self.sessions.session_reply(reply_content["content"], session_id, 100)
                 return Reply(ReplyType.TEXT, reply_content)
             else:
                 flag = self.check_cookie()
